ab7221b65238efc6dd5382554215706ab4612bc8
Adoniro Salles <adonirosalles@gmail.com>
2021-08-14 00:19:36 -0300

    aula 4 ajustes hoome

--- lib/widgets/GridProdutos/elemento_gridprodutos.dart
+++ lib/widgets/GridProdutos/elemento_gridprodutos.dart
@@ -11,15 +11,30 @@ class ElementoGridProtudos extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        ImageElementoGridProdutos(imagem: movel.foto),
-        DegradeGrideProdutos(),
-        TituloElementoGridProdutos(
-          titulo: movel.titulo,
-        )
-      ],
+    return Container(
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            spreadRadius: 2,
+            blurRadius: 8,
+            color: Colors.black12,
+          ),
+        ],
+      ),
+      margin: EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            ImageElementoGridProdutos(imagem: movel.foto),
+            DegradeGrideProdutos(),
+            TituloElementoGridProdutos(
+              titulo: movel.titulo,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
